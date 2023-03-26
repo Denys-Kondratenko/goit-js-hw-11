3ae9234275330a6e8cdc36098e22c9c7eee71178
Denys-Kondratenko <dk132652@gmail.com>
2023-03-26 21:45:18 +0300

    закінчив ДЗ без додаткових завдань

--- src/fethPictures.js
+++ src/fethPictures.js
@@ -1,9 +1,9 @@
 import axois from 'axios';
 
-export async function fethPictures(request) {
+export async function fethPictures(request, page) {
   try {
     const response = await axois.get(
-      `https://pixabay.com/api/?key=34726411-d79d2bb382e9b3b825be3cd38&q=${request}&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?key=34726411-d79d2bb382e9b3b825be3cd38&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     );
     // console.log(response);
     return response.data;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import { fethPictures } from './fethPictures';
+import { Notify } from 'notiflix';
 import './css/styles.css';
 
 let request = '';
+let page = 1;
+let calcHits = 0;
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  btnMore: document.querySelector('.load-more'),
+};
 
 function template({
   webformatURL,
@@ -35,17 +44,30 @@ function template({
 </div>`;
 }
 
-const getPictures = async () => {
-  const { total, totalHits, hits } = await fethPictures(request);
-  const markup = hits.map(elem => template(elem)).join('');
-  console.log(markup);
+const getPictures = async page => {
+  try {
+    const { totalHits, hits } = await fethPictures(request, page);
+    const markup = hits.map(elem => template(elem)).join('');
+    console.log(totalHits, hits);
+    calcHits += hits.length;
 
-  refs.gallery.innerHTML = markup;
-};
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+    refs.btnMore.style.display = 'block';
 
-const refs = {
-  form: document.querySelector('.search-form'),
-  gallery: document.querySelector('.gallery'),
+    if (hits.length === 0) {
+      refs.btnMore.style.display = 'none';
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else if (calcHits === totalHits) {
+      refs.btnMore.style.display = 'none';
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 refs.form.addEventListener('submit', handleSubmit);
@@ -53,6 +75,23 @@ refs.form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   request = event.currentTarget.elements.searchQuery.value;
-  // console.log(event.currentTarget.elements.searchQuery.value);
+
+  if (request === '') {
+    return;
+  }
+
+  calcHits = 0;
+  refs.btnMore.style.display = 'none';
+  refs.gallery.innerHTML = '';
+
   getPictures();
+
+  page = 1;
+}
+
+refs.btnMore.addEventListener('click', handleMore);
+
+function handleMore(event) {
+  page += 1;
+  getPictures(page);
 }
